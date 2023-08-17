6193c00454e60d96288337c404e2bf03355e2f2d
Alexey Illarionov <alexey@0xdc.ru>
2023-08-17 13:08:00 +0300

    Upgrade gradle to 8.3, Kotlin to 1.9.0

--- libraries/build.gradle.kts
+++ libraries/build.gradle.kts
@@ -3,12 +3,3 @@ plugins {
 }
 
 group = "ru.hh.plugins.build_logic"
-
-kotlin {
-    @Suppress("MagicNumber")
-    jvmToolchain(17)
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
-}
