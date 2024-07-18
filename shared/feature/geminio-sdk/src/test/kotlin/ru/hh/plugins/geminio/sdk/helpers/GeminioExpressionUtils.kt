a091a5ac6bed0879dfeb272df4148fe43b364eec
Alexey Illarionov <alexey@0xdc.ru>
2024-07-18 23:58:33 +0300

    Fix lint

--- shared/feature/geminio-sdk/src/test/kotlin/ru/hh/plugins/geminio/sdk/helpers/GeminioExpressionUtils.kt
+++ shared/feature/geminio-sdk/src/test/kotlin/ru/hh/plugins/geminio/sdk/helpers/GeminioExpressionUtils.kt
@@ -51,7 +51,7 @@ internal object GeminioExpressionUtils {
         return ModuleTemplateData(
             projectTemplateData = ProjectTemplateData(
                 androidXSupport = true,
-                agpVersion = AgpVersion(8,0),
+                agpVersion = AgpVersion(8, 0),
                 sdkDir = File("/AndroidSdk"),
                 language = Language.Kotlin,
                 kotlinVersion = "1.4.10",
